7b2a6b873aad3129b5b64dfad8f5c4a43b077f7e
Agasi Miroyan <limekick@mail.ru>
2024-03-18 05:31:26 +0400

    3.3 end2

--- 3.3/1.py
+++ 3.3/1.py
@@ -11,23 +11,27 @@ async def boil_water(time: int):
 
 async def chop_vedgetables():
     print('Начинаем нарезку овощей...')
-    await asyncio.sleep(randint(2, 4))
-    print("Овощи готовы")
+    x = await asyncio.sleep(randint(2, 4))
+    print(f"Овощи готовы за {x}")
 
 
 async def prepare_dinner():
 
     #----вариант 1: короткий----
 
-    # await asyncio.gather(boil_water(5), chop_vedgetables())
+    await asyncio.gather(boil_water(5), chop_vedgetables())
 
      #----вариант 2: длинный----
 
-    task1 = asyncio.create_task(boil_water(5))
-    task2 = asyncio.create_task(chop_vedgetables())
+    # task1 = asyncio.create_task(boil_water(5))
+    # task2 = asyncio.create_task(chop_vedgetables())
 
-    await task1
-    await task2
+    # await task1
+    # await task2
 
+    #----синхронное выполнение функций---
+
+    # await boil_water(5)
+    # await chop_vedgetables()
 
 asyncio.run(prepare_dinner())
